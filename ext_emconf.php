e0997c3280e3f47d62b21b1326baedf1a3ef02be
CONVERS Sébastien <sebastien.convers@agrosupdijon.fr>
2020-07-30 17:26:22 +0200

    [TASK] version 1.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -41,5 +41,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sébastien Convers',
     'author_email' => 'sebastien.convers@agrosupdijon.fr',
     'author_company' => 'AgroSup Dijon',
-    'version' => '1.1.0',
+    'version' => '1.1.1',
 ];
