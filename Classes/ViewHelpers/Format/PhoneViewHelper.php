c436d3a096c18fa000aafb93302fd61cef0b694e
CONVERS Sébastien <sebastien.convers@agrosupdijon.fr>
2021-06-23 15:36:20 +0200

    [FEATURE] Add a view helper to format phone numbers

---
+++ Classes/ViewHelpers/Format/PhoneViewHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the package agrosup-dijon/bulma-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace AgrosupDijon\BulmaPackage\ViewHelpers\Format;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+/**
+ * PhoneViewHelper
+ */
+class PhoneViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     *
+     * @return string the formatted amount
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        // Get rid of everything between parentheses (parentheses included)
+        $firstReplace = preg_replace('/\((\X+?)\)/', '', $renderChildrenClosure());
+
+        // Get rid of everything that is not a digit or a + character in first position
+        return preg_replace('/(?!(^\+))\D+/', '', $firstReplace);
+    }
+}
