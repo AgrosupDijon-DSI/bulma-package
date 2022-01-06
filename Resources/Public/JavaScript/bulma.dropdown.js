5a8f2dc0de1fb7b316797f6d56ca2b04943c1599
CONVERS Sébastien <sebastien.convers@agrosupdijon.fr>
2022-01-06 16:53:36 +0100

    Manage navbar dropdown overflow

--- Resources/Public/JavaScript/bulma.dropdown.js
+++ Resources/Public/JavaScript/bulma.dropdown.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    var dropdowns = document.querySelectorAll('.dropdown');
-    var isOutOfViewport = function (elem) {
+    const dropdowns = document.querySelectorAll('.dropdown');
+    const hoverableNavbarDropdowns = document.querySelectorAll('.navbar-item.has-dropdown.is-hoverable:not(.is-mega)');
 
+    const isOutOfViewport = function (elem) {
         // Get element's bounding
-        var bounding = elem.getBoundingClientRect();
+        let bounding = elem.getBoundingClientRect();
 
         // Check if it's out of the viewport on each side
-        var out = {};
+        let out = {};
         out.top = bounding.top < 0;
         out.left = bounding.left < 0;
         out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
@@ -16,40 +17,66 @@ document.addEventListener('DOMContentLoaded', () => {
         out.all = out.top && out.left && out.bottom && out.right;
 
         return out;
-
     };
 
-    dropdowns.forEach(function (dropdown, index) {
+    function fixPositionX(dropdownElement, positionerElement){
+        let isOut = isOutOfViewport(dropdownElement);
 
+        if(isOut.right && !isOut.left){
+            positionerElement.classList.add('is-right');
+            if(isOutOfViewport(dropdownElement).left){
+                positionerElement.classList.remove('is-right');
+            }
+        }
+        if(isOut.left && !isOut.right){
+            positionerElement.classList.remove('is-right');
+            if(isOutOfViewport(dropdownElement).left){
+                positionerElement.classList.add('is-right');
+            }
+        }
+    }
+
+    function fixPositionY(dropdownElement, positionerElement, className = 'is-up'){
+        let isOut = isOutOfViewport(dropdownElement);
+
+        if (isOut.bottom && !isOut.top) {
+            positionerElement.classList.add(className);
+            if(isOutOfViewport(dropdownElement).top){
+                positionerElement.classList.remove(className);
+            }
+        }
+        if (isOut.top && !isOut.bottom) {
+            positionerElement.classList.remove(className);
+            if(isOutOfViewport(dropdownElement).bottom){
+                positionerElement.classList.add(className);
+            }
+        }
+    }
+
+    hoverableNavbarDropdowns.forEach(function (dropdown, index) {
+        let dropdownMenu = dropdown.children[1];
+        const originalDropdownClasses = dropdown.className;
+        const originalDropdownMenuClasses = dropdownMenu.className;
+
+        dropdown.addEventListener('mouseenter', () => {
+            fixPositionX(dropdownMenu, dropdownMenu);
+            fixPositionY(dropdownMenu, dropdown, 'has-dropdown-up');
+        });
+
+        dropdown.addEventListener('mouseleave', () => {
+            dropdown.className = originalDropdownClasses;
+            dropdownMenu.className = originalDropdownMenuClasses;
+        });
+    });
+
+    dropdowns.forEach(function (dropdown, index) {
         const originalClasses = dropdown.className;
+        let dropdownMenu = dropdown.children[1];
 
         dropdown.addEventListener('click', () => {
             dropdown.classList.toggle('is-active');
-            var isOut = isOutOfViewport(dropdown.children[1]);
-            if(isOut.right && !isOut.left){
-                dropdown.classList.add('is-right');
-                if(isOutOfViewport(dropdown.children[1]).left){
-                    dropdown.classList.remove('is-right');
-                }
-            }
-            if(isOut.left && !isOut.right){
-                dropdown.classList.remove('is-right');
-                if(isOutOfViewport(dropdown.children[1]).right){
-                    dropdown.classList.add('is-right');
-                }
-            }
-            if (isOut.bottom && !isOut.top) {
-                dropdown.classList.add('is-up');
-                if(isOutOfViewport(dropdown.children[1]).top){
-                    dropdown.classList.remove('is-up');
-                }
-            }
-            if (isOut.top && !isOut.bottom) {
-                dropdown.classList.remove('is-up');
-                if(isOutOfViewport(dropdown.children[1]).bottom){
-                    dropdown.classList.add('is-up');
-                }
-            }
+            fixPositionX(dropdownMenu, dropdown);
+            fixPositionY(dropdownMenu, dropdown);
         });
 
         window.addEventListener('click', function(e){
@@ -58,4 +85,5 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
+
 });
