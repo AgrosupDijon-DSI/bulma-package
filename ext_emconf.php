aa85f9a0b90c9b6a35551d4d379f88f1e514a1d6
CONVERS Sébastien <sebastien.convers@agrosupdijon.fr>
2022-06-13 11:03:36 +0200

    [TASK] Raise version to 1.8.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -39,5 +39,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sébastien Convers',
     'author_email' => 'sebastien.convers@agrosupdijon.fr',
     'author_company' => 'AgroSup Dijon',
-    'version' => '1.7.0',
+    'version' => '1.8.0',
 ];
