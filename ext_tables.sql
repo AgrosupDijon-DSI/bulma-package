9f974e64bb9fe6cf99276b0b6e0e98eab9c30316
CONVERS Sébastien <sebastien.convers@agrosupdijon.fr>
2023-07-13 16:24:38 +0200

    [TASK] Remove general fields from ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,9 +3,6 @@
 #
 CREATE TABLE tx_bulmapackage_settings
 (
-    uid                                int(11)                         NOT NULL auto_increment,
-    pid                                int(11)             DEFAULT '0' NOT NULL,
-
     title_seo                          varchar(255)        DEFAULT ''  NOT NULL,
     logo_main                          int(11) unsigned    DEFAULT '0' NOT NULL,
     logos_partners                     int(11) unsigned    DEFAULT '0' NOT NULL,
@@ -22,32 +19,7 @@ CREATE TABLE tx_bulmapackage_settings
     latitude                           varchar(255)        DEFAULT ''  NOT NULL,
     longitude                          varchar(255)        DEFAULT ''  NOT NULL,
     tx_bulmapackage_settings_link_item int(11) unsigned    DEFAULT '0',
-    menu_layout                        varchar(30)         DEFAULT ''  NOT NULL,
-
-    tstamp                             int(11) unsigned    DEFAULT '0' NOT NULL,
-    crdate                             int(11) unsigned    DEFAULT '0' NOT NULL,
-    cruser_id                          int(11) unsigned    DEFAULT '0' NOT NULL,
-    deleted                            tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    hidden                             tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-    t3ver_oid                          int(11)             DEFAULT '0' NOT NULL,
-    t3ver_id                           int(11)             DEFAULT '0' NOT NULL,
-    t3ver_wsid                         int(11)             DEFAULT '0' NOT NULL,
-    t3ver_label                        varchar(255)        DEFAULT ''  NOT NULL,
-    t3ver_state                        tinyint(4)          DEFAULT '0' NOT NULL,
-    t3ver_stage                        int(11)             DEFAULT '0' NOT NULL,
-    t3ver_count                        int(11)             DEFAULT '0' NOT NULL,
-    t3ver_tstamp                       int(11)             DEFAULT '0' NOT NULL,
-    t3ver_move_id                      int(11)             DEFAULT '0' NOT NULL,
-
-    sys_language_uid                   int(11)             DEFAULT '0' NOT NULL,
-    l10n_parent                        int(11)             DEFAULT '0' NOT NULL,
-    l10n_diffsource                    mediumblob,
-
-    PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY t3ver_oid (t3ver_oid, t3ver_wsid),
-    KEY language (l10n_parent, sys_language_uid)
+    menu_layout                        varchar(30)         DEFAULT ''  NOT NULL
 );
 
 #
@@ -55,9 +27,6 @@ CREATE TABLE tx_bulmapackage_settings
 #
 CREATE TABLE tx_bulmapackage_custom_color
 (
-    uid           int(11)                       NOT NULL auto_increment,
-    pid           int(11)           DEFAULT '0' NOT NULL,
-
     label         varchar(255)      DEFAULT ''  NOT NULL,
     var_primary   varchar(255)      DEFAULT ''  NOT NULL,
     var_link      varchar(255)      DEFAULT ''  NOT NULL,
@@ -65,29 +34,7 @@ CREATE TABLE tx_bulmapackage_custom_color
     var_info      varchar(255)      DEFAULT ''  NOT NULL,
     var_warning   varchar(255)      DEFAULT ''  NOT NULL,
     var_danger    varchar(255)      DEFAULT ''  NOT NULL,
-    var_text_dark smallint unsigned DEFAULT '0' NOT NULL,
-
-    tstamp        int(11) unsigned  DEFAULT '0' NOT NULL,
-    crdate        int(11) unsigned  DEFAULT '0' NOT NULL,
-    cruser_id     int(11) unsigned  DEFAULT '0' NOT NULL,
-    deleted       smallint unsigned DEFAULT '0' NOT NULL,
-    hidden        smallint unsigned DEFAULT '0' NOT NULL,
-    sorting       int(11)           DEFAULT '0' NOT NULL,
-
-    t3ver_oid     int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_id      int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_wsid    int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_label   varchar(255)      DEFAULT ''  NOT NULL,
-    t3ver_state   smallint          DEFAULT '0' NOT NULL,
-    t3ver_stage   int(11)           DEFAULT '0' NOT NULL,
-    t3ver_count   int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_tstamp  int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_move_id int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3_origuid    int(11) unsigned  DEFAULT '0' NOT NULL,
-
-    PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY t3ver_oid (t3ver_oid, t3ver_wsid)
+    var_text_dark smallint unsigned DEFAULT '0' NOT NULL
 );
 
 #
@@ -95,33 +42,8 @@ CREATE TABLE tx_bulmapackage_custom_color
 #
 CREATE TABLE tx_bulmapackage_meta_tags
 (
-    uid           int(11)                       NOT NULL auto_increment,
-    pid           int(11)           DEFAULT '0' NOT NULL,
-
     name          varchar(255)      DEFAULT ''  NOT NULL,
-    content       varchar(255)      DEFAULT ''  NOT NULL,
-
-    tstamp        int(11) unsigned  DEFAULT '0' NOT NULL,
-    crdate        int(11) unsigned  DEFAULT '0' NOT NULL,
-    cruser_id     int(11) unsigned  DEFAULT '0' NOT NULL,
-    deleted       smallint unsigned DEFAULT '0' NOT NULL,
-    hidden        smallint unsigned DEFAULT '0' NOT NULL,
-    sorting       int(11)           DEFAULT '0' NOT NULL,
-
-    t3ver_oid     int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_id      int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_wsid    int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_label   varchar(255)      DEFAULT ''  NOT NULL,
-    t3ver_state   smallint          DEFAULT '0' NOT NULL,
-    t3ver_stage   int(11)           DEFAULT '0' NOT NULL,
-    t3ver_count   int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_tstamp  int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_move_id int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3_origuid    int(11) unsigned  DEFAULT '0' NOT NULL,
-
-    PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY t3ver_oid (t3ver_oid, t3ver_wsid)
+    content       varchar(255)      DEFAULT ''  NOT NULL
 );
 
 #
@@ -129,9 +51,6 @@ CREATE TABLE tx_bulmapackage_meta_tags
 #
 CREATE TABLE tx_bulmapackage_settings_link_item
 (
-    uid                      int(11) unsigned                NOT NULL auto_increment,
-    pid                      int(11)             DEFAULT '0' NOT NULL,
-
     tx_bulmapackage_settings int(11) unsigned    DEFAULT '0',
     label                    varchar(255)        DEFAULT ''  NOT NULL,
     link                     varchar(1024)       DEFAULT ''  NOT NULL,
@@ -139,34 +58,7 @@ CREATE TABLE tx_bulmapackage_settings_link_item
     icon_file                int(11) unsigned    DEFAULT '0',
     force_label              tinyint(4) unsigned DEFAULT '0' NOT NULL,
     standalone               tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    icon_custom              tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-    tstamp                   int(11) unsigned    DEFAULT '0' NOT NULL,
-    crdate                   int(11) unsigned    DEFAULT '0' NOT NULL,
-    cruser_id                int(11) unsigned    DEFAULT '0' NOT NULL,
-    deleted                  smallint unsigned   DEFAULT '0' NOT NULL,
-    hidden                   smallint unsigned   DEFAULT '0' NOT NULL,
-    sorting                  int(11)             DEFAULT '0' NOT NULL,
-
-    sys_language_uid         int(11)             DEFAULT '0' NOT NULL,
-    l10n_parent              int(11) unsigned    DEFAULT '0' NOT NULL,
-    l10n_diffsource          mediumblob                      NULL,
-
-    t3ver_oid                int(11) unsigned    DEFAULT '0' NOT NULL,
-    t3ver_id                 int(11) unsigned    DEFAULT '0' NOT NULL,
-    t3ver_wsid               int(11) unsigned    DEFAULT '0' NOT NULL,
-    t3ver_label              varchar(255)        DEFAULT ''  NOT NULL,
-    t3ver_state              smallint            DEFAULT '0' NOT NULL,
-    t3ver_stage              int(11)             DEFAULT '0' NOT NULL,
-    t3ver_count              int(11) unsigned    DEFAULT '0' NOT NULL,
-    t3ver_tstamp             int(11) unsigned    DEFAULT '0' NOT NULL,
-    t3ver_move_id            int(11) unsigned    DEFAULT '0' NOT NULL,
-    t3_origuid               int(11) unsigned    DEFAULT '0' NOT NULL,
-
-    PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY t3ver_oid (t3ver_oid, t3ver_wsid),
-    KEY language (l10n_parent, sys_language_uid)
+    icon_custom              tinyint(4) unsigned DEFAULT '0' NOT NULL
 );
 
 #
@@ -212,40 +104,9 @@ CREATE TABLE pages
 #
 CREATE TABLE tx_bulmapackage_accordion_item
 (
-    uid              int(11)                         NOT NULL auto_increment,
-    pid              int(11)             DEFAULT '0' NOT NULL,
-
     tt_content       int(11)             DEFAULT '0' NOT NULL,
     record           int(11) unsigned    DEFAULT '0' NOT NULL,
-    title            tinytext,
-
-    tstamp           int(11) unsigned    DEFAULT '0' NOT NULL,
-    crdate           int(11) unsigned    DEFAULT '0' NOT NULL,
-    cruser_id        int(11) unsigned    DEFAULT '0' NOT NULL,
-    deleted          tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    hidden           tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    starttime        int(11) unsigned    DEFAULT '0' NOT NULL,
-    endtime          int(11) unsigned    DEFAULT '0' NOT NULL,
-
-    sorting          int(11) unsigned    DEFAULT '0' NOT NULL,
-    t3ver_oid        int(11) unsigned    DEFAULT '0' NOT NULL,
-    t3ver_id         int(11)             DEFAULT '0' NOT NULL,
-    t3ver_wsid       int(11)             DEFAULT '0' NOT NULL,
-    t3ver_label      varchar(255)        DEFAULT ''  NOT NULL,
-    t3ver_state      tinyint(4)          DEFAULT '0' NOT NULL,
-    t3ver_stage      int(11)             DEFAULT '0' NOT NULL,
-    t3ver_count      int(11)             DEFAULT '0' NOT NULL,
-    t3ver_tstamp     int(11)             DEFAULT '0' NOT NULL,
-    t3ver_move_id    int(11)             DEFAULT '0' NOT NULL,
-
-    sys_language_uid int(11)             DEFAULT '0' NOT NULL,
-    l10n_parent      int(11) unsigned    DEFAULT '0' NOT NULL,
-    l10n_diffsource  mediumblob,
-
-    PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY t3ver_oid (t3ver_oid, t3ver_wsid),
-    KEY language (l10n_parent, sys_language_uid)
+    title            tinytext
 );
 
 #
@@ -253,40 +114,9 @@ CREATE TABLE tx_bulmapackage_accordion_item
 #
 CREATE TABLE tx_bulmapackage_tab_item
 (
-    uid              int(11)                         NOT NULL auto_increment,
-    pid              int(11)             DEFAULT '0' NOT NULL,
-
     tt_content       int(11)             DEFAULT '0' NOT NULL,
     record           int(11) unsigned    DEFAULT '0' NOT NULL,
-    title            varchar(255)        DEFAULT ''  NOT NULL,
-
-    tstamp           int(11) unsigned    DEFAULT '0' NOT NULL,
-    crdate           int(11) unsigned    DEFAULT '0' NOT NULL,
-    cruser_id        int(11) unsigned    DEFAULT '0' NOT NULL,
-    deleted          tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    hidden           tinyint(4) unsigned DEFAULT '0' NOT NULL,
-    starttime        int(11) unsigned    DEFAULT '0' NOT NULL,
-    endtime          int(11) unsigned    DEFAULT '0' NOT NULL,
-
-    sorting          int(11) unsigned    DEFAULT '0' NOT NULL,
-    t3ver_oid        int(11) unsigned    DEFAULT '0' NOT NULL,
-    t3ver_id         int(11)             DEFAULT '0' NOT NULL,
-    t3ver_wsid       int(11)             DEFAULT '0' NOT NULL,
-    t3ver_label      varchar(255)        DEFAULT ''  NOT NULL,
-    t3ver_state      tinyint(4)          DEFAULT '0' NOT NULL,
-    t3ver_stage      int(11)             DEFAULT '0' NOT NULL,
-    t3ver_count      int(11)             DEFAULT '0' NOT NULL,
-    t3ver_tstamp     int(11)             DEFAULT '0' NOT NULL,
-    t3ver_move_id    int(11)             DEFAULT '0' NOT NULL,
-
-    sys_language_uid int(11)             DEFAULT '0' NOT NULL,
-    l10n_parent      int(11) unsigned    DEFAULT '0' NOT NULL,
-    l10n_diffsource  mediumblob,
-
-    PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY t3ver_oid (t3ver_oid, t3ver_wsid),
-    KEY language (l10n_parent, sys_language_uid)
+    title            varchar(255)        DEFAULT ''  NOT NULL
 );
 
 
@@ -295,45 +125,13 @@ CREATE TABLE tx_bulmapackage_tab_item
 #
 CREATE TABLE tx_bulmapackage_card_group_item
 (
-    uid              int(11) unsigned              NOT NULL auto_increment,
-    pid              int(11)           DEFAULT '0' NOT NULL,
-
     tt_content       int(11) unsigned  DEFAULT '0',
     header           varchar(255)      DEFAULT ''  NOT NULL,
     subheader        varchar(255)      DEFAULT ''  NOT NULL,
     media            int(11)           DEFAULT '0' NOT NULL,
     bodytext         text,
     link             varchar(1024)     DEFAULT ''  NOT NULL,
-    link_title       varchar(255)      DEFAULT ''  NOT NULL,
-
-    tstamp           int(11) unsigned  DEFAULT '0' NOT NULL,
-    crdate           int(11) unsigned  DEFAULT '0' NOT NULL,
-    cruser_id        int(11) unsigned  DEFAULT '0' NOT NULL,
-    deleted          smallint unsigned DEFAULT '0' NOT NULL,
-    hidden           smallint unsigned DEFAULT '0' NOT NULL,
-    starttime        int(11) unsigned  DEFAULT '0' NOT NULL,
-    endtime          int(11) unsigned  DEFAULT '0' NOT NULL,
-    sorting          int(11)           DEFAULT '0' NOT NULL,
-
-    sys_language_uid int(11)           DEFAULT '0' NOT NULL,
-    l10n_parent      int(11) unsigned  DEFAULT '0' NOT NULL,
-    l10n_diffsource  mediumblob                    NULL,
-
-    t3ver_oid        int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_id         int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_wsid       int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_label      varchar(255)      DEFAULT ''  NOT NULL,
-    t3ver_state      smallint          DEFAULT '0' NOT NULL,
-    t3ver_stage      int(11)           DEFAULT '0' NOT NULL,
-    t3ver_count      int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_tstamp     int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_move_id    int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3_origuid       int(11) unsigned  DEFAULT '0' NOT NULL,
-
-    PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY t3ver_oid (t3ver_oid, t3ver_wsid),
-    KEY language (l10n_parent, sys_language_uid)
+    link_title       varchar(255)      DEFAULT ''  NOT NULL
 );
 
 #
@@ -341,9 +139,6 @@ CREATE TABLE tx_bulmapackage_card_group_item
 #
 CREATE TABLE tx_bulmapackage_carousel_item
 (
-    uid              int(11) unsigned              NOT NULL auto_increment,
-    pid              int(11)           DEFAULT '0' NOT NULL,
-
     tt_content       int(11) unsigned  DEFAULT '0',
     item_type        varchar(255)      DEFAULT ''  NOT NULL,
     header           varchar(255)      DEFAULT ''  NOT NULL,
@@ -353,36 +148,7 @@ CREATE TABLE tx_bulmapackage_carousel_item
     button_class     varchar(255)      DEFAULT ''  NOT NULL,
     link             varchar(1024)     DEFAULT ''  NOT NULL,
     background_color varchar(255)      DEFAULT ''  NOT NULL,
-    image            int(11) unsigned  DEFAULT '0',
-
-    tstamp           int(11) unsigned  DEFAULT '0' NOT NULL,
-    crdate           int(11) unsigned  DEFAULT '0' NOT NULL,
-    cruser_id        int(11) unsigned  DEFAULT '0' NOT NULL,
-    deleted          smallint unsigned DEFAULT '0' NOT NULL,
-    hidden           smallint unsigned DEFAULT '0' NOT NULL,
-    starttime        int(11) unsigned  DEFAULT '0' NOT NULL,
-    endtime          int(11) unsigned  DEFAULT '0' NOT NULL,
-    sorting          int(11)           DEFAULT '0' NOT NULL,
-
-    sys_language_uid int(11)           DEFAULT '0' NOT NULL,
-    l10n_parent      int(11) unsigned  DEFAULT '0' NOT NULL,
-    l10n_diffsource  mediumblob                    NULL,
-
-    t3ver_oid        int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_id         int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_wsid       int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_label      varchar(255)      DEFAULT ''  NOT NULL,
-    t3ver_state      smallint          DEFAULT '0' NOT NULL,
-    t3ver_stage      int(11)           DEFAULT '0' NOT NULL,
-    t3ver_count      int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_tstamp     int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_move_id    int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3_origuid       int(11) unsigned  DEFAULT '0' NOT NULL,
-
-    PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY t3ver_oid (t3ver_oid, t3ver_wsid),
-    KEY language (l10n_parent, sys_language_uid)
+    image            int(11) unsigned  DEFAULT '0'
 );
 
 #
@@ -390,9 +156,6 @@ CREATE TABLE tx_bulmapackage_carousel_item
 #
 CREATE TABLE tx_bulmapackage_icon_group_item
 (
-    uid              int(11) unsigned              NOT NULL auto_increment,
-    pid              int(11)           DEFAULT '0' NOT NULL,
-
     tt_content       int(11) unsigned  DEFAULT '0',
     bodytext         mediumtext,
     link             varchar(1024)     DEFAULT ''  NOT NULL,
@@ -400,34 +163,5 @@ CREATE TABLE tx_bulmapackage_icon_group_item
     icon_set         varchar(255)      DEFAULT ''  NOT NULL,
     icon_file        int(11) unsigned  DEFAULT '0',
     icon_size        varchar(255)      DEFAULT ''  NOT NULL,
-    icon_color       varchar(255)      DEFAULT ''  NOT NULL,
-
-    tstamp           int(11) unsigned  DEFAULT '0' NOT NULL,
-    crdate           int(11) unsigned  DEFAULT '0' NOT NULL,
-    cruser_id        int(11) unsigned  DEFAULT '0' NOT NULL,
-    deleted          smallint unsigned DEFAULT '0' NOT NULL,
-    hidden           smallint unsigned DEFAULT '0' NOT NULL,
-    starttime        int(11) unsigned  DEFAULT '0' NOT NULL,
-    endtime          int(11) unsigned  DEFAULT '0' NOT NULL,
-    sorting          int(11)           DEFAULT '0' NOT NULL,
-
-    sys_language_uid int(11)           DEFAULT '0' NOT NULL,
-    l10n_parent      int(11) unsigned  DEFAULT '0' NOT NULL,
-    l10n_diffsource  mediumblob                    NULL,
-
-    t3ver_oid        int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_id         int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_wsid       int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_label      varchar(255)      DEFAULT ''  NOT NULL,
-    t3ver_state      smallint          DEFAULT '0' NOT NULL,
-    t3ver_stage      int(11)           DEFAULT '0' NOT NULL,
-    t3ver_count      int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_tstamp     int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3ver_move_id    int(11) unsigned  DEFAULT '0' NOT NULL,
-    t3_origuid       int(11) unsigned  DEFAULT '0' NOT NULL,
-
-    PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY t3ver_oid (t3ver_oid, t3ver_wsid),
-    KEY language (l10n_parent, sys_language_uid)
+    icon_color       varchar(255)      DEFAULT ''  NOT NULL
 );
