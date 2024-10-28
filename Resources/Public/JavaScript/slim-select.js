c78efcf926db0ea322bd09e7114d0db5dd9e3e5e
CONVERS Sébastien <sebastien.convers@agrosupdijon.fr>
2024-10-28 16:27:04 +0100

    [BUGFIX] Fix Slim Select instanciation with multiple MultiSelect

--- Resources/Public/JavaScript/slim-select.js
+++ Resources/Public/JavaScript/slim-select.js
@@ -1,11 +1,16 @@
 import SlimSelect from 'slim-select'
 
-new SlimSelect({
-    select: 'select[multiple=multiple]',
-    settings: {
-        closeOnSelect: false,
-        showSearch: false,
-        hideSelected: true,
-        // placeholderText: 'Choisissez un ou plusieurs sujets'
-    },
-})
+document.addEventListener('DOMContentLoaded', () => {
+    if( document.querySelector('select[multiple]') !== null ) {
+        document.querySelectorAll('select[multiple]').forEach(selectMultipleElement => {
+            new SlimSelect({
+                select: selectMultipleElement,
+                settings: {
+                    closeOnSelect: false,
+                    showSearch: false,
+                    hideSelected: true,
+                }
+            })
+        });
+    }
+});
