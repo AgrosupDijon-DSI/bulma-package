0c8385f28aaf01afec34e8293694ed34a9ef35ef
CONVERS Sébastien <sebastien.convers@agrosupdijon.fr>
2024-10-30 16:36:16 +0100

    [TASK] Add viewhelper to retrieve logo_main from WebsiteSettings in backend context (Experimental)

---
+++ Classes/ViewHelpers/WebsiteSettings/MainLogoViewHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AgrosupDijon\BulmaPackage\ViewHelpers\WebsiteSettings;
+
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Resource\FileRepository;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * @internal May change / vanish any time
+ */
+class MainLogoViewHelper extends AbstractViewHelper
+{
+    public function __construct(
+        private readonly ConnectionPool $connectionPool,
+        private readonly FileRepository $fileRepository,
+    ) {}
+
+    public function render()
+    {
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable('tx_bulmapackage_settings');
+        $result = $queryBuilder
+            ->select('uid', 'pid', 'sys_language_uid', 'logo_main')
+            ->from('tx_bulmapackage_settings')
+            ->executeQuery()
+            ->fetchAssociative();
+
+        /** @var FileReference[] $files */
+        $files = $this->fileRepository->findByRelation('tx_bulmapackage_settings', 'logo_main', $result['uid']);
+
+        return $files[0];
+    }
+}
