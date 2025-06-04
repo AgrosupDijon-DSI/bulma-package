c168d1d9d40a8ca37670c4a1c901a5b4ae927c50
CONVERS Sébastien <sebastien.convers@agrosupdijon.fr>
2025-06-04 17:57:56 +0200

    [TASK] Upgrade modules to follow dependabot alert on http-proxy-middleware

--- Resources/Public/JavaScript/simplebar.js
+++ Resources/Public/JavaScript/simplebar.js
@@ -1,5 +1,5 @@
 import 'simplebar';
-import 'simplebar/dist/simplebar.css';
+import 'simplebar/dist/simplebar.min.css';
 
 import ResizeObserver from 'resize-observer-polyfill';
 window.ResizeObserver = ResizeObserver;
