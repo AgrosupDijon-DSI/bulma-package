9913281fd81e76e2120751947d56a48e36373a26
CONVERS Sébastien <sebastien.convers@agrosupdijon.fr>
2026-07-15 15:48:14 +0200

    [TASK] Add some styling to content site update

--- Configuration/TCA/Overrides/250_content_element_site_update.php
+++ Configuration/TCA/Overrides/250_content_element_site_update.php
@@ -16,9 +16,9 @@ ExtensionManagementUtility::addRecordType(
     ],
     '
         --palette--;;headers,
+        --palette--;LLL:EXT:bulma_package/Resources/Private/Language/Backend.xlf:palette.siteupdatelayout;siteupdatelayout,
         --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
-            --palette--;LLL:EXT:bulma_package/Resources/Private/Language/Backend.xlf:palette.siteupdatelayout;siteupdatelayout,
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
             categories
@@ -28,5 +28,5 @@ ExtensionManagementUtility::addRecordType(
 ExtensionManagementUtility::addFieldsToPalette(
     'tt_content',
     'siteupdatelayout',
-    'table_header_position'
+    'message_class, table_header_position'
 );
